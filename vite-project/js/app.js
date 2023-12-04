c3a27806499efb14d15b1ff888fd97aac90ebedb
Kelly Ng <kellyng19lover@gmail.com>
2023-12-04 10:15:53 -0500

    yi

---
+++ vite-project/js/app.js
@@ -0,0 +1,20 @@
+
+const URL= `https://recipal.com/api/`;
+// async goes before any function that grabs api
+async function getData(URL){
+    try{
+        //requesting a response REST API's
+        const response = await fetch(URL);
+        if(response.stauts !=200){
+            throw new Error(response.statusText);
+        }
+        //convert reponse to json
+        const data= await response.json();
+        document.querySelector("h1").textContent = data.Content;
+        //displays if there is error
+    } catch (error){
+        conslone.log(error, "uh on");
+        document.querySelector("h1").textContent = "uh oh"
+    }
+}
+getData(URL);
