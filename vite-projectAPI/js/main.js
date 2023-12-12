a3f97809e9bbac585580bc15e36dfddc1652deb0
Kelly Ng <kellyng19lover@gmail.com>
2023-12-12 10:15:33 -0500

    fh
    fh

--- vite-projectAPI/js/main.js
+++ vite-projectAPI/js/main.js
@@ -5,6 +5,8 @@ import { setupCounter } from './counter.js'
 
 const URL= `https://api.spoonacular.com/recipes/random`;
 const key= 'ddc432ea9f6b4256ac7ec8f75ae3b67f';
+
+const url=[]
 // async goes before any function that grabs api
 async function getData(URL){
     try{
@@ -20,6 +22,7 @@ async function getData(URL){
         //convert reponse to json
         const data= await response.json();
         console.log(data);
+        url.push(data);
         //displays if there is error
     } catch (error){
         console.log(error);
@@ -28,13 +31,18 @@ async function getData(URL){
 }
 getData(URL);
 
-function insert(arr){
-    arr.forEach((data) => {
+
+function insert(url){
+    url.forEach((data) => {
 DOMSelectors.container_2.insertAdjacentHTML(
     "afterbegin",
     `<div class= "card">
     <h2 class= "title">${data.title}</h2>
     <img scr="${data.image}" class="cardimg">
-    <h3 class= "readyInMinutes" id= >${data.readyInMinutes}</h3>
+    <h3 class=" cuisines" >${"cuisines"+ data.cuisines}</h3>
+    <h3 class= "readyInMinutes" >${"Minutes"+data.readyInMinutes}</h3>
     </div>`
     )})};
+
+insert(arr);
+
