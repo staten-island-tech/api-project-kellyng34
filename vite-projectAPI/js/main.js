c482dd1ce86a6f4642476e0e03ee41b676581d1e
Kelly Ng <kellyng19lover@gmail.com>
2024-01-05 09:47:45 -0500

    Revert "rh"
    This reverts commit ce276b467408fc997f151c4f3ab39012703e35e5.

--- vite-projectAPI/js/main.js
+++ vite-projectAPI/js/main.js
@@ -6,46 +6,20 @@ import { setupCounter } from './counter.js'
 
 const URL = `https://api.spoonacular.com/recipes/random`;
 const key = 'ddc432ea9f6b4256ac7ec8f75ae3b67f';
-
-let all = []
 async function init(){
     let data = []
     function insert(data) {
         console.log(data)
-        data.forEach((x) => { 
-            const card= document.createElement("div")
-            card.className="card"
-            card.innerHTML=      
-        `<h2 class= "title">${x.title}</h2>
+        data.forEach((x) => {
+            DOMSelectors.container.insertAdjacentHTML(
+                "afterbegin",
+                `<div class= "card">
+        <h2 class= "title">${x.title}</h2>
         <img src="${x.image}" class="cardimg">
         <h3 class=" cuisines" >${x.cuisines}</h3>
-        <h3 class= "readyInMinutes" >${"Time to prepare: " + x.readyInMinutes + " minutes"}</h3>`
-            
-        all.push(card)
-        
-        DOMSelectors.container.addEventListener("click", function(e){
-            e.preventDefault;
-            clear(DOMSelectors.card)
-            const id = x.id;
-            const URL_2= `https://api.spoonacular.com/recipes/${id}/tasteWidget.json`
-
-            async function getDataa(URL_2) {
-                try {
-                    //requesting a response REST API's
-                    const response = await fetch
-            
-                    if (response.status != 200) {
-                        throw new Error(response.statusText);
-                    }
-                    //convert reponse to json
-                    const info_2 = await response.json();
-            
-                } catch (error) {
-                    console.log(error);
-                    document.querySelector("h1").textContent = "uh fbroh";
-                }}
-                getDataa(URL_2)
-        })
+        <h3 class= "readyInMinutes" >${"Time to prepare: " + x.readyInMinutes + " minutes"}</h3>
+        </div>`
+            )
         })
     };
     //push the data into the data array
@@ -66,35 +40,56 @@ async function init(){
             console.log(info);
     
             info.recipes.forEach((recipes) => data.push(recipes));
-            const id = data.forEach((data)=> data.id);
-            console.log(id);
+    
     
         } catch (error) {
             console.log(error);
-            document.querySelector("h1").textContent = "uhfgjhhg oh";
+            document.querySelector("h1").textContent = "uh oh";
         }
     }
     await getData(URL).then(()=>{
         insert(data);
     })
+    
+    // const URL_2= `https://api.spoonacular.com/recipes/${id}/tasteWidget.json`
+    // async function getData(URL_2) {
+    //     try {
+    //         //requesting a response REST API's
+    //         const response = await fetch(URL_2 + "?" + new URLSearchParams({
+    //             apiKey: key,
+    //             number: 39
+    
+    //         }));
+    //         if (response.status != 200) {
+    //             throw new Error(response.statusText);
+    //         }
+    //         //convert reponse to json
+    //         const info = await response.json();
+    //         console.log(info);
+    
+    //     } catch (error) {
+    //         console.log(error);
+    //         document.querySelector("h1").textContent = "uh oh";
+    //     }}
+    
 
+    let all = document.querySelectorAll(".card")
 
     function clear(card){
-       card="<div class=`card`></div>";
-       console.log(card)
-        card.innerHTML=
-            `<h3 class= "sweetness" ></h3>
-            <h3 class= "saltiness" ></h3>
-            <h3 class= "bitterness" ></h3>
-            <h3 class= "savoriness" ></h3>
-            <h3 class= "spiciness" ></h3>`
-            
+       card.innerHTML="";
+        card.insertAdjacentHTML( 
+            "afterbegin", 
+            `<h3 class="sweetness" ></h3>
+            <h3 class="saltiness" ></h3>
+            <h3 class="bitterness" ></h3>
+            <h3 class="savoriness" ></h3>
+            <h3 class="spiciness" ></h3>`
+            )
     }
     
     all.forEach((card) => card.addEventListener("click",function(event){
         clear(card)
     }))
-    document.addEventListener("DOMContentLoaded")
 
 }
 
